a85dae63dc969367fd7ddb2747b9f686cfbb7167
Liline <lilinelady@gmail.com>
2014-12-16 00:03:44 -0800

    c

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 var express = require("express");
 var request = require('request');
 
+
 var api = '633cbe7f-4b19-41eb-8d57-c13d04b85b71';
 
 var url = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/37?champData=skins&api_key='+api;
 
 var app = express();
 
+//Set the view directory to /views
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs"); //tell express we're using EJS
+
+
+//use jade templating language
+/*app.set("view engine", "jade");*/
+
 console.log("Hello World " + api);
 
-app.get('/', function (req,res){
-
-	res.render('index.html', {pattern:null});
+app.get('/', function(req, res) {
+	apicall(res);
+ 	
 });
 
-app.get('/riotweb/riot.html', function(req, res) {
-  request(url, function(error,response,body) {
+app.post('/search', function(req,res){
+
+})
+
+
+function apicall(res){
+
+	 request(url, function(error,response,body) {
 	if (!error && response.statusCode==200){
 		console.log('body ' + body);
-		res.send(body);
+		res.render("index", body);
 	}
-
-});
-
-});
-
-
-
-
+})
+}
 
 
 app.listen(3001)
